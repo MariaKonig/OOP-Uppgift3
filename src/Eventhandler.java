98d7bd23479f870f937bf407ac8429af0329b192
Maria König <maria.konig@yh.nackademin.se>
2023-10-28 20:24:33 +0200

    fixed diagonal move

--- src/Eventhandler.java
+++ src/Eventhandler.java
@@ -11,42 +11,54 @@ public class Eventhandler implements ActionListener {
     List<JButton> list;
     JPanel pl;
     updateGame update = new updateGame();
-    public Eventhandler(JButton pressedButton, List<JButton> list, JPanel panel){
+
+    public Eventhandler(JButton pressedButton, List<JButton> list, JPanel panel) {
         button = pressedButton;
         this.list = list;
         pl = panel;
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         int i = list.indexOf(button);
         int j = getValidMove(i);
-        if(!(j == -1)) {
+        if (!(j == -1)) {
             swap(list, i, j);
             update.updatePanel();
         }
 
     }
 
-    public int getValidMove(int i){
-        if( !(i==0) && list.get(i - 1).getText().isEmpty()){ //kolla vänster
-            return i-1;
+    public int getValidMove(int i) {
+        int leftB = i - 1;
+        int rightB = i + 1;
+        int downB = i + 4;
+        int uppB = i - 4;
+
+        if (!(i == 0) && list.get(leftB).getText().isEmpty()) { //kolla vänster
+            if(!(leftB==3) && !(leftB == 7) && !(leftB == 11)){
+                return leftB;
+            }
         }
-        else if(((i+1)<16) && list.get(i+1).getText().isEmpty()){ //kolla höger
-            return i+1;
+        else if (((rightB) < 16) && list.get(rightB).getText().isEmpty()) { //kolla höger
+            if (!(rightB==4) && !(rightB==8) && !(rightB == 12)){
+                return rightB;
+            }
         }
-        else if ((i-4)>0 && list.get(i-4).getText().isEmpty()) { //kolla uppe
-            return i-4;
-        }else if((i+4)<16 && list.get(i+4).getText().isEmpty()){ // kolla nere
-            return i+4;
+        else if ((uppB) >= 0 && list.get(uppB).getText().isEmpty()) { //kolla uppe
+            return uppB;
+        }
+        else if ((downB) < 16 && list.get(downB).getText().isEmpty()) { // kolla nere
+            return downB;
         }
         return -1;
     }
 
-    private class updateGame{
+    private class updateGame {
 
-        public void updatePanel(){
+        public void updatePanel() {
             pl.removeAll();
-            for(JButton button :  list){
+            for (JButton button : list) {
                 pl.add(button);
             }
             pl.repaint();
