7e2b09911e3b066dc1c3000ceec9488cf89e490e
Maria König <maria.konig@yh.nackademin.se>
2023-10-28 15:53:51 +0200

    Initial Commit

---
+++ src/SifferSpel.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class SifferSpel  extends JFrame {
+
+
+
+
+
+    
+    public SifferSpel(){
+
+    }
+
+
+    public static void main(String[] args) {
+        SifferSpel k = new SifferSpel();
+    }
+}
