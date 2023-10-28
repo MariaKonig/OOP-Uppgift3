7a55666ff0e83d522ae6d692790a44ff1db7dfe3
Maria König <maria.konig@yh.nackademin.se>
2023-10-28 18:46:47 +0200

    removed class & added method to swap buttons

--- src/Eventhandler.java
+++ src/Eventhandler.java
@@ -1,14 +1,33 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
+
+import static java.util.Collections.swap;
 
 public class Eventhandler implements ActionListener {
     JButton button;
-    public Eventhandler(JButton pressedButton){
+    List<JButton> list;
+    JPanel pl;
+    public Eventhandler(JButton pressedButton, List<JButton> list, JPanel panel){
         button = pressedButton;
+        this.list = list;
+        pl = panel;
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        int i = list.indexOf(button);
+        int j = i+1;
+        swap(list,  i, j);
+        updatePanel();
 
+    }
+    public void updatePanel(){
+        pl.removeAll();
+        for(JButton button :  list){
+            pl.add(button);
+        }
+        pl.repaint();
+        pl.revalidate();
     }
 }
